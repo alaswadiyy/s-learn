f92f01df1f70247fd89a067f40c1470831afd581
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-08-11 00:48:01 +0100

    navbar

---
+++ src/components/Navbar/NavLink.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const NavLink = () => {
+  return (
+    <div>NavLink</div>
+  )
+}
+
+export default NavLink
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { navLinks } from '../../Data'
+import { HiMenuAlt1, HiX } from "react-icons/hi";
+import NavLink from './NavLink';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full top-0 left-0 z-20'>
+        <div>
+            <div className='container py-4 mx-auto flex items-center justify-between px-2'>
+                <div className='flex items-center gap-4'>
+                    <HiMenuAlt1 className='text-3xl sm:hidden cursor-pointer'/>
+                    <div className='text-xl text-teal uppercase tracking-wide font-bold'>S-Learn</div>
+                </div>
+                <div>
+                    {navLinks.map(() => {
+                        // eslint-disable-next-line react/jsx-key
+                        return <NavLink/>
+                    })}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
